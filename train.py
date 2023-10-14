5ef6e6b47e3b53d0bb857e1819aac82ac9f33750
siyeong0 <sy1211w@gmail.com>
2023-10-14 23:09:02 +0900

    training script

---
+++ train.py
@@ -0,0 +1,20 @@
+from isaacgym import gymapi
+
+from mtss_gym.utils.helpers import class_to_dict, get_log_dir
+
+from rsl_rl.runners import OnPolicyRunner
+
+from mtss_gym.envs.mtss import MotionTrackingFromSparseSensor
+from mtss_gym.envs.mtss_cfg import MtssCfg, MtssPPOCfg
+
+if __name__ == "__main__":
+    env = MotionTrackingFromSparseSensor(MtssCfg(), gymapi.SIM_PHYSX, "cuda:0", False)
+
+    ppo_runner = OnPolicyRunner(env, class_to_dict(MtssPPOCfg), get_log_dir("logs", "mtss"), device="cuda:0")
+    ppo_runner.learn(num_learning_iterations=MtssPPOCfg.runner.max_iterations, init_at_random_ep_len=True)
+    
+    obs = env.get_observations()
+    
+    while True:
+        actions = env.sample_action()
+        obs, _, rews, dones, infos = env.step(actions)  
