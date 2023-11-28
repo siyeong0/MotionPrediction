44e1c6cf0d2c5d8a685a674ab7d728446101f9ae
siyeong0 <sy1211w@gmail.com>
2023-11-28 17:24:57 +0900

    log analysis

---
+++ log_anly.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from mtss_gym.utils.log import parse_log_file
+
+if __name__ == "__main__":
+    data = parse_log_file("logs/r0/log.txt")
+    
+    x_label = "Total timesteps"
+    y_label = "Mean step reward"
+    x_list = []
+    y_list = []
+    for d in data:
+        x_list.append(d[x_label])
+        y_list.append(d[y_label])
+            
+    plt.plot(np.array(x_list), np.array(y_list))
+    plt.show()
